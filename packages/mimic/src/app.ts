1c78918c35c348bf02c3507c9553f26fb286050c
Dipesh Yadav <maxdipesh@gmail.com>
2019-11-17 19:20:33 +0200

    Respond with json error when route not found

--- packages/mimic/src/app.ts
+++ packages/mimic/src/app.ts
@@ -47,6 +47,7 @@ export function createApp({ templateDir }: Options): Output {
     });
 
     app.use('/', router);
+
     // catch 404 and forward to error handler
     app.use((req: Request, res: Response, next: NextFunction) => {
         next(createError(404));
@@ -54,13 +55,12 @@ export function createApp({ templateDir }: Options): Output {
 
     // error handler
     app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-
         // render the error page
         res.status(err.status || 500);
-        res.render('error');
+        res.json({
+            message: err.message,
+            error: req.app.get('env') === 'development' ? err : {},
+        });
     });
 
     return {
